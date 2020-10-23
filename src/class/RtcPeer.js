5a2d5b85e9a9c17b3bcf98b2d3bd7d6078d46443
zexho <zouzhihao994@gmail.com>
2020-10-23 19:18:47 +0800

    add RtcPeer.js

---
+++ src/class/RtcPeer.js
@@ -0,0 +1,116 @@
+'use strict';
+
+import {getRawPeerName, iceServer, screenSuffix} from "../index";
+
+/**
+ * 在收到自己Joined事件时,创建OFFER
+ * peer为自己的RTCPeerConnection对象
+ *
+ * @param peerName 对方peer的名称
+ * @param pc RTCPeerConnection {@link RTCPeerConnection}
+ * @param client Client类 {@link Client}
+ * @param socketServer SocketServer类
+ */
+function createOffer(peerName, pc, client, socketServer) {
+    console.log(pc, "create offer to", peerName)
+    pc.createOffer({
+        // offerToReceiveAudio:1,
+        offerToReceiveVideo: 1
+    }).then((desc) => {
+        pc.setLocalDescription(desc, () => {
+            console.log(peerName, "设置local description")
+            socketServer.emitOffer(peerName, pc, client.roomId)
+        }, (err) => {
+            console.log('create offer Error]', err)
+        })
+    })
+}
+
+/**
+ * 获取屏幕共享流
+ * @param p 对端peer {peerName,remoteScreen} {@link #Socket#onJoined peer变量}
+ * @param client 客户端对象 {@class Client}
+ * @param socketServer socket对象
+ */
+function getScreenConnection(p, client, socketServer) {
+    let pc = new RTCPeerConnection(iceServer);
+
+    // 如果检测到对方媒体流连接，将其绑定到一个video上
+    pc.ontrack = (event) => {
+        console.log("接收 track", pc)
+        // 存在流
+        if (event.streams) {
+            console.log("存在stream", event)
+            let screenStream = event.streams[0];
+            let screenTrack = screenStream.getTracks()[0]
+            screenTrack.onmute = () => {
+                try {
+                    client.onRemoveScreenStream();
+                } catch (e) {
+                    console.error('[Caller error] onRemoveScreenStream,', error)
+                }
+            }
+            try {
+                let account = getRawPeerName(p.remoteScreenName.split(screenSuffix)[0], client.account)
+                client.addRemoteScreen(account, screenStream)
+            } catch (e) {
+                console.error('[Caller error] onRemoteScreenStream', e)
+            }
+        }
+    }
+
+    // 发送ICE给其他客户端
+    pc.onicecandidate = (event) => {
+        if (event.candidate) {
+            socketServer.emitIceCandidate(event.candidate, client.roomId, p.remoteScreenName)
+        }
+    }
+
+    // 设置监听
+    pc.onnegotiationneeded = (event) => {
+        createOffer(p.peerName, pc, client, socketServer)
+    }
+
+    // 添加远端
+    client.addRemoteScreen(p.remoteScreenName, pc)
+
+}
+
+/**
+ * 创建对端pc，然后设置回调函数
+ * @param p 对端peer {peerName,remoteScreen} {@link #Socket#onJoined peer变量}
+ * @param client 本客户端 {@link Client}
+ * @param socketServer socket服务类
+ */
+function getPeerConnection(p, client, socketServer) {
+
+    let pc = new RTCPeerConnection(iceServer);
+    console.log("create peer connection ", pc)
+
+    // 如果检测到对方媒体流连接，将其绑定到一个video标签上输出
+    pc.ontrack = (event) => {
+        console.log("接收到track",pc)
+        if (event.streams) {
+            socketServer.onTrack(p, event, client)
+        }
+    }
+
+    // 发送icecandidate信息时给p
+    pc.onicecandidate = (event) => {
+        console.log("接收到icecandidate",pc)
+        if (event.candidate) {
+            socketServer.emitIceCandidate(event.candidate, client.roomId, p.peerName)
+        }
+    }
+
+    // 当发生协议变更时
+    pc.onnegotiationneeded = () => {
+        createOffer(p.peerName, pc, client, socketServer)
+    }
+
+    // 保存对端名称
+    client.addPeer(p.peerName, pc)
+    console.log("getPeerConnection ok")
+}
+
+export {createOffer, getScreenConnection, getPeerConnection}
