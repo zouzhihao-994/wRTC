4635e4a78e356cd8de3f664bb9004c807957f28b
zexho <zouzhihao994@gmail.com>
2020-10-28 13:23:14 +0800

    添加 RtcApi 类，提供主动方法调用

---
+++ src/api.js
@@ -0,0 +1,134 @@
+'use strict'
+
+class RtcApi {
+
+    /**
+     * 初始化客户端
+     * @param option
+     */
+    init(option) {
+
+    }
+
+    /**
+     * 加入房间
+     * @param account
+     * @param roomId
+     */
+    joinRoom(account, roomId) {
+
+    }
+
+    /**
+     * 离开房间
+     */
+    leaveRoom(account, roomId) {
+
+    }
+
+    /**
+     * 播放视频
+     * @param elementId
+     * @param options
+     */
+    play(elementId, options) {
+
+    }
+
+    /**
+     * 设置是否允许发布音频流
+     */
+    configLocalAudioPublish() {
+
+    }
+
+    /**
+     * 设置是否允许发布视频流
+     */
+    configLocalCameraPublish() {
+
+    }
+
+    /**
+     * 设置是否允许发布屏幕共享流
+     */
+    configLocalScreenPublish() {
+
+    }
+
+    /**
+     * 发布本地流
+     */
+    publish() {
+
+    }
+
+
+    unPublish() {
+
+    }
+
+    /**
+     * 是否订阅远端音频流
+     */
+    configRemoteAudio() {
+
+    }
+
+    /**
+     * 是否订阅远端相机流
+     */
+    configRemoteCameraTrack() {
+
+    }
+
+    /**
+     * 是否订阅远端屏幕流
+     */
+    configRemoteScreenTrack() {
+
+    }
+
+    /**
+     * 订阅远端流
+     */
+    subscribe() {
+
+    }
+
+    /**
+     * 取消订阅远端流
+     */
+    unSubscribe() {
+
+    }
+
+    /**
+     * 预览本地摄像头
+     */
+    startPreview() {
+
+    }
+
+    /**
+     * 结束预览本地摄像头
+     */
+    stopPreview() {
+
+    }
+
+    onJoined(){
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
