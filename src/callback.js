40f4962c6de375eb1437384384ee11102ceb9434
zexho <zouzhihao994@gmail.com>
2020-10-28 15:11:31 +0800

    更新 callback.js

--- src/callback.js
+++ src/callback.js
@@ -1,31 +1,53 @@
 'use strict'
 
 // 回调函数
+import {client} from "./index";
+
 let callback = {
+    onJoin: null,
+
     /**
      * 房间里其他人发布本地流时
      * @return account:发布者的account
      */
-    onPublisher: null,
+    onPublisher: function (account, stream) {
+        console.log(">>> ", new Date().toLocaleTimeString(), " [info] callback -> onPublisher, account = {},stream = {}", account, stream)
+    },
 
     /**
      * 当频道里其他人取消发布本地流时
      * @return account 取消者的account
      */
-    onUnPublisher: null,
+    onUnPublisher: function (account) {
+        console.log(">>> ", new Date().toLocaleTimeString(), " [info] callback -> onUnPublisher, account = {},stream = {}", account, stream)
+    },
 
     /**
      * 订阅远程流成功时触发stream
      * @return subscriber 远程流的参数
      * @return stream 远程的流
      */
-    onMediaStream: null,
+    onAudioTrack: null,
 
+    onVideoTrack: null,
+
+    /**
+     * 房间有新的远程屏幕流
+     * @param account 发起者account
+     * @param stream 屏幕流
+     */
+    onScreenStream: function (account, stream) {
+        console.log(">>> ", new Date().toLocaleTimeString(), " [info] callback -> onScreenTrack, account = {},stream = {}", account, stream)
+    },
 
     /**
      * 房间有人离开时
      * @return account 离开者的account
      */
-    onLeave: null
+    onLeave: function (account) {
+        console.log(">>> ", new Date().toLocaleTimeString(), " [info] callback -> onLeave, account = {}, stream = {}", account)
+    }
 
 }
+
+export {callback}
